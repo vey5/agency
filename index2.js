2e46e65d4657c4e1a699dd59ec161b640d2175db
vey5 <vlad.9505@mail.ru>
2023-03-24 22:24:43 +0300

    обновил задачки

--- index2.js
+++ index2.js
@@ -1,5 +1,5 @@
 //1 задача - Написать объект ladder - объект, который позволяет подниматься вверх и спускаться.
-const ladder = {
+/*const ladder = {
     step: 0,
     up ()  {
         console.log(this.step)
@@ -21,5 +21,21 @@ ladder.up()//0
 ladder.up()//1
 ladder.showStep()//2
 ladder.down()//2
-ladder.showStep()//1
- 
+ladder.showStep()//1*/
+ //2 задача - найти числа, которые делятся на заданное число
+ // вроде все правильно, но divisor is not defined, пока оставляю
+ /*numbers = [1, 2, 3, 4, 5, 6]
+ const findDivisibleBy = (numbers, divisor) => {
+  const divisibleBy = numbers.filter(function(number) {
+   const div = number % divisor  === 0
+    return div
+  })
+  return divisibleBy
+ }
+ console.log(findDivisibleBy(numbers, divisor))*/
+ //3 задача - посчитать нечетные числа ниже n 
+ const oddCount = (n) => {
+  const odd = (n-1) / 2
+  return odd
+ }
+ console.log(oddCount(15))
